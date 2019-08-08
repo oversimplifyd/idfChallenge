d42af996c824cac171ba7dfdbc178de434bf3095
Arotimi Busayo <arotimi.busayo@gmail.com>
2019-08-08 07:38:56 +0100

    Moved queries into repository for CoursEnrolmentController

--- app/Http/Controllers/CourseEnrollmentController.php
+++ app/Http/Controllers/CourseEnrollmentController.php
@@ -3,24 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Course;
-use App\CourseEnrollment;
+use App\Repositories\CourseRepoistory;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Response;
+use App\Repositories\CourseEnrollmentRepository;
 
 class CourseEnrollmentController extends Controller
 {
+
+    private $courseEnrolmentRepo;
+    private $courseRepo;
+
+    public function __construct(CourseEnrollmentRepository $ceRepo, CourseRepoistory $courseRepo)
+    {
+        $this->courseEnrolmentRepo = $ceRepo;
+        $this->courseRepo = $courseRepo;
+    }
+
     public function show(string $slug): Renderable
     {
         /** @var Course $course */
-        $course = Course::query()
-                ->where('slug', $slug)
-                ->first() ?? abort(Response::HTTP_NOT_FOUND, 'Course not found');
+        $course = $this->courseRepo
+                ->findBy('slug', $slug) ??
+            abort(Response::HTTP_NOT_FOUND, 'Course not found');
 
-        $enrollment = CourseEnrollment::query()
-            ->where('course_id', $course->id)
-            ->where('user_id', auth()->id())
-            ->with('course.lessons')
-            ->first();
+        $enrollment = $this->courseEnrolmentRepo->getUserLessons($course->id, auth()->id);
 
         if ($enrollment === null) {
             return view('courses.show', ['course' => $course]);
@@ -32,9 +39,9 @@ class CourseEnrollmentController extends Controller
     public function store(string $slug)
     {
         /** @var Course $course */
-        $course = Course::query()
-                ->where('slug', $slug)
-                ->first() ?? abort(Response::HTTP_NOT_FOUND, 'Course not found');
+        $course = $this->courseRepo
+                ->findBy('slug', $slug) ??
+            abort(Response::HTTP_NOT_FOUND, 'Course not found');
 
         $course->enroll(auth()->user());
 
--- app/Repository/CourseEnrollmentRepository.php
+++ app/Repository/CourseEnrollmentRepository.php
@@ -5,11 +5,20 @@ namespace App\Repositories;
 use App\CourseEnrollment;
 use App\Repository\Eloquent\Repository as Repo;
 
-class CourseRepoistory extends Repo
+class CourseEnrollmentRepository extends Repo
 {
 
     public function model()
     {
         return CourseEnrollment::class;
+    }
+
+    public function getUserLessons(int $courseId, int $userId)
+    {
+        return ($this->model())
+            ->where('course_id', $courseId)
+            ->where('user_id', $userId)
+            ->with('course.lessons')
+            ->first();
     }
 }
