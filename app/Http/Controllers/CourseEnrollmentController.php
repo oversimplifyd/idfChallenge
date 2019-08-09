7dc564d7dca3d5a59b859abb4448faf2e04fe900
Arotimi Busayo <arotimi.busayo@gmail.com>
2019-08-09 07:48:46 +0100

    Added leaderboard grouping logic

--- app/Http/Controllers/CourseEnrollmentController.php
+++ app/Http/Controllers/CourseEnrollmentController.php
@@ -11,6 +11,9 @@ use App\Repositories\CourseEnrollmentRepository;
 class CourseEnrollmentController extends Controller
 {
 
+    private const MAX_DISPLAY_ITEM = 9;
+    private const MIN_DISPLAY_ITEM = 3;
+
     private $courseEnrolmentRepo;
     private $courseRepo;
 
@@ -23,6 +26,9 @@ class CourseEnrollmentController extends Controller
     public function show(string $slug): Renderable
     {
         /** @var Course $course */
+        $board = $this->courseRepo->getCourseLeaderboard($slug);
+        $leaderBoard = $this->reGroupUsers($board->toArray());
+
         $course = $this->courseRepo
                 ->findBy('slug', $slug) ??
             abort(Response::HTTP_NOT_FOUND, 'Course not found');
@@ -46,5 +52,89 @@ class CourseEnrollmentController extends Controller
         $course->enroll(auth()->user());
 
         return redirect()->action([self::class, 'show'], [$course->slug]);
+    }
+
+    /**
+     * @param array $courseUsers
+     */
+    private function reGroupUsers(array $courseUsers)
+    {
+        $leaderBoardCount = count($courseUsers);
+        $result = [];
+        if ($leaderBoardCount >= self::MIN_DISPLAY_ITEM) {
+            $result['lowest'] = $this->getLowest($courseUsers, self::MIN_DISPLAY_ITEM);
+            $result['highest'] = $this->getHighest($courseUsers, self::MIN_DISPLAY_ITEM);
+
+            if ($leaderBoardCount >= self::MAX_DISPLAY_ITEM) {
+                $result['list'] = $this->getDisplayItem($courseUsers, self::MAX_DISPLAY_ITEM);
+            } else {
+                $result['list'] = $this->getDisplayItem($courseUsers, $leaderBoardCount);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $board
+     * @param int $count
+     * @return array
+     */
+    private function getHighest(array $board, int $count)
+    {
+        $result = [];
+        for ($i = 0; $i < $count; $i++) {
+            $result[] = $board[$i];
+        }
+
+        return $this->reArrange($result);
+    }
+
+    /**
+     * @param array $board
+     * @param int $count
+     */
+    private function getLowest(array $board, int $count)
+    {
+        $boardCount = count($board);
+        $result = [];
+        for ($i = 1; $i <= $count; $i++) {
+            $result[] = $board[$boardCount - $i];
+        }
+
+        return $this->reArrange($result);
+    }
+
+    /**
+     * @param array $board
+     * @param int $count
+     */
+    private function getDisplayItem(array $board, int $count)
+    {
+        $result = [];
+        for ($i = 0; $i < $count; $i++) {
+            $result[] = $board[$i];
+        }
+
+        return $this->reArrange($result);
+    }
+
+    /**
+     * @param array $result
+     * @return array
+     */
+    private function reArrange(array $result)
+    {
+        $loggedInUserId = auth()->user()->id;
+        usort($result, function($a, $b) use($loggedInUserId) {
+            if ($a->score === $b->score) {
+                if ($b->userid === $loggedInUserId) {
+                    return 1;
+                }
+            }
+            return 0;
+        });
+
+        return $result;
     }
 }
--- app/Repository/CourseRepoistory.php
+++ app/Repository/CourseRepoistory.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Course;
 use App\Repository\Eloquent\Repository as Repo;
+use Illuminate\Support\Facades\DB;
 
 class CourseRepoistory extends Repo
 {
@@ -11,5 +12,19 @@ class CourseRepoistory extends Repo
     public function model()
     {
         return Course::class;
+    }
+
+    public function getCourseLeaderboard(string $slug)
+    {
+        return DB::table('courses')
+            ->join('lessons', 'courses.id', '=', 'lessons.course_id')
+            ->join('quizzes', 'lessons.id', '=', 'quizzes.lesson_id')
+            ->join('quiz_answers', 'quizzes.id', '=', 'quiz_answers.quiz_id')
+            ->join('users', 'quiz_answers.user_id', '=', 'users.id')
+            ->select(DB::raw('SUM(quiz_answers.score) as score'),'users.name', 'users.id as userid', 'courses.id as courseid', 'courses.title')
+            ->where('slug', $slug)
+            ->groupBy('users.name', 'users.id', 'courses.id', 'courses.title')
+            ->orderBy('score', 'desc')
+            ->get();
     }
 }
